f1c5c05ec8891e364c285aee7ec69b9ea3148906
Phil Newton <phil@sodaware.net>
2014-11-29 14:32:12 -0500

    Changed spec descriptions from "it should be able to" to "can"

--- spec/the_shining_source/request_spec.rb
+++ spec/the_shining_source/request_spec.rb
@@ -6,7 +6,7 @@ describe TheShiningSource::Request do
     attr_accessor :format, :user_agent, :proxy, :connection_options, :endpoint
     attr_accessor :adapter, :access_token, :mime_type
     
-    def initialize()
+    def initialize
       @format = :json
       @endpoint = 'http://api.shiningsource.org/1.0/'
       @adapter = Faraday.default_adapter
@@ -19,39 +19,39 @@ describe TheShiningSource::Request do
     @stub.extend TheShiningSource::Connection
   end
   
-  it 'should raise an exception for invalid HTTP methods' do
+  it 'raises an exception for invalid HTTP methods' do
     expect { @stub.request(:invalid, '/', {}, {}) }.to raise_error(ArgumentError)
   end
   
-  it "should be able to make GET requests" do
+  it 'can make GET requests' do
     stub_get('').to_return(body: '', headers: { content_type: 'text/plain' })
     
     @stub.get_request('/')
     expect(a_get('')).to have_been_made
   end
   
-  it 'should be able to make POST requests' do
+  it 'can make POST requests' do
     stub_post('').to_return(body: '', headers: { content_type: 'text/plain' })
     
     @stub.post_request('/')
     expect(a_post('')).to have_been_made
   end
   
-  it 'should be able to make PUT requests' do
+  it 'can make PUT requests' do
     stub_put('').to_return(body: '', headers: { content_type: 'text/plain' })
     
     @stub.put_request('/')
     expect(a_put('')).to have_been_made
   end
 
-  it 'should be able to make PATCH requests' do
+  it 'can make PATCH requests' do
     stub_patch('').to_return(body: '', headers: { content_type: 'text/plain' })
     
     @stub.patch_request('/')
     expect(a_patch('')).to have_been_made
   end
   
-  it 'should be able to make DELETE requests' do
+  it 'can make DELETE requests' do
     stub_delete('').to_return(body: '', headers: { content_type: 'text/plain' })
     
     @stub.delete_request('/')
