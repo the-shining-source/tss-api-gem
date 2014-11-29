adbd9cd6486c8c446073772b82e4aa166a96b200
Phil Newton <phil@sodaware.net>
2014-11-29 13:35:05 -0500

    Added new project gallery endpoint wrappers

--- lib/the_shining_source/projects/galleries.rb
+++ lib/the_shining_source/projects/galleries.rb
@@ -1,10 +1,22 @@
 module TheShiningSource
   class Projects::Galleries < ApiBase
-    
+
+    ##
+    # Get a single gallery for a project
     def get(project, name)
-      get_request("/projects/#{project}/galleries/#{name}/")
+      gallery = get_request("/projects/#{project}/galleries/#{name}/")
+      TheShiningSource::Gallery.new(gallery)
     end
-    alias :find :get
-    
+    alias_method :find, :get
+    alias_method :find_by_slug, :get
+
+    ##
+    # Get all galleries for +project+.
+    def list(project)
+      galleries = get_request("/projects/#{project}/galleries/")
+      galleries.map { |gallery| TheShiningSource::Gallery.new(gallery) }
+    end
+    alias_method :all, :list
+
   end
 end
