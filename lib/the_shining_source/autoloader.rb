d840361deebdd714842dec406263a8a642a7a119
Phil Newton <phil@sodaware.net>
2014-11-29 14:56:42 -0500

    Removed unused code from autoloader

--- lib/the_shining_source/autoloader.rb
+++ lib/the_shining_source/autoloader.rb
@@ -7,15 +7,5 @@ module TheShiningSource
       end
     end
     
-    def register_constant(options)
-      options.each do |const_name, value|
-        const_set const_name.upcase.to_s, value
-      end
-    end
-    
-    def lookup_constant(const_name)
-      const_get const_name.upcase.to_s
-    end
-    
   end
 end
