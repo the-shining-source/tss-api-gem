74520012e2d175dfb09c8f4c308549752ed3a675
Phil Newton <phil@sodaware.net>
2014-11-29 13:34:10 -0500

    Split downloads/get_spec into get/list spec files

--- spec/the_shining_source/projects/downloads/get_spec.rb
+++ spec/the_shining_source/projects/downloads/get_spec.rb
@@ -6,32 +6,6 @@ describe TheShiningSource::Projects::Downloads, '#get' do
     @client = TheShiningSource::Client.new
   end
 
-  describe 'GET => /projects/{name}/downloads/' do
-    
-    before do
-      stub_get('projects/shining-online/downloads/', 'projects/downloads/list.json')
-    end
-
-    it 'should get all downloads for a single project' do
-      @client.projects.downloads.list('shining-online')
-      expect(a_get('projects/shining-online/downloads/')).to have_been_made
-    end
-    
-    it 'should return an array of Download objects' do
-      downloads = @client.projects.downloads.list('shining-online')
-      expect(downloads).to be_a(Array)
-      downloads.each do |download|
-        expect(download).to be_a TheShiningSource::Download
-      end
-    end
-
-    it 'should support the `all` alias' do
-      @client.projects.downloads.all('shining-online')
-      expect(a_get('projects/shining-online/downloads/')).to have_been_made
-    end
-    
-  end
-  
   describe 'GET => /projects/{name}/downloads/{download}/' do
     
     before do
--- spec/the_shining_source/projects/downloads/list_spec.rb
+++ spec/the_shining_source/projects/downloads/list_spec.rb
@@ -5,23 +5,31 @@ describe TheShiningSource::Projects::Downloads, '#list' do
   before do 
     @client = TheShiningSource::Client.new
   end
-  
+
   describe 'GET => /projects/{name}/downloads/' do
     
     before do
       stub_get('projects/shining-online/downloads/', 'projects/downloads/list.json')
-    end    
-    
-    it 'should get a list of downloads for a project' do
+    end
+
+    it 'should get all downloads for a single project' do
       @client.projects.downloads.list('shining-online')
       expect(a_get('projects/shining-online/downloads/')).to have_been_made
     end
     
-    it 'should fetch an array of downloads for a project' do
+    it 'should return an array of Download objects' do
       downloads = @client.projects.downloads.list('shining-online')
       expect(downloads).to be_a(Array)
+      downloads.each do |download|
+        expect(download).to be_a TheShiningSource::Download
+      end
+    end
+
+    it 'should support the `all` alias' do
+      @client.projects.downloads.all('shining-online')
+      expect(a_get('projects/shining-online/downloads/')).to have_been_made
     end
     
-  end  
+  end
   
 end
