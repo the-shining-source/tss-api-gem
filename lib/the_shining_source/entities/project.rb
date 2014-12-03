fdc52ab7cf63527c2d0d0ab5fe0163a192546cb1
Phil Newton <phil@sodaware.net>
2014-12-03 18:28:17 -0500

    Added missing project fields

--- lib/the_shining_source/entities/project.rb
+++ lib/the_shining_source/entities/project.rb
@@ -4,8 +4,9 @@ module TheShiningSource
   # Holds information about a single Project
   class Project < EntityBase
     attr_accessor :id, :name, :url, :slug, :created, :modified, :last_update,
-                  :homepage, :progress, :summary, :description, :banners,
-                  :downloads, :team_members
+                  :homepage, :progress, :summary, :description,
+                  :twitter_name, :facebook_page, :google_plus,
+                  :banners, :galleries, :downloads, :team_members
   end
   
 end
