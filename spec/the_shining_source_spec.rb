e4d8549c682903f20952d22a51e7d0bd745f33f3
Phil Newton <phil@sodaware.net>
2014-12-27 14:42:08 -0500

    Cleaned up TheShiningSource class specs

--- spec/the_shining_source_spec.rb
+++ spec/the_shining_source_spec.rb
@@ -2,64 +2,68 @@ require 'spec_helper'
 
 describe TheShiningSource do
 
-  it "should respond to 'new' message" do
+  it "responds to 'new' message" do
     expect(subject).to respond_to :new
   end
 
-  it "should receive 'new' and initialize subject::Client instance" do
+  it "receives 'new' and initialize subject::Client instance" do
     expect(subject.new).to be_a TheShiningSource::Client
   end
   
-  it "should respond to 'configure' message" do
+  it "responds to 'configure' message" do
     expect(subject).to respond_to :configure
   end
 
   describe 'delegating to the client' do
 
-    it 'should respond_to if method exists Client' do
+    it 'calls respond_to if method exists in Client class' do
       expect(TheShiningSource).to respond_to(:projects)
     end
 
-    it 'should delegate missing methods to Client' do
+    it 'delegates missing methods to Client' do
       expect(TheShiningSource.projects).to be_a(TheShiningSource::Projects)
     end
 
   end
-  
-  describe 'setting configuration options' do
-    
-    it 'should return default adapter' do
+
+  describe 'after creation' do
+ 
+    it 'returns default adapter' do
       expect(subject.adapter).to eq TheShiningSource::Configuration::DEFAULT_ADAPTER
     end
-    
-    it 'should allow to set adapter' do
-      subject.adapter = :typhoeus
-      expect(subject.adapter).to eq :typhoeus
-    end
-    
-    it 'should return the default end point' do
+ 
+    it 'returns the default end point' do
       expect(subject.endpoint).to eq TheShiningSource::Configuration::DEFAULT_ENDPOINT
     end
 
-    it 'should allow to set endpoint' do
-      subject.endpoint = 'http://example.org'
-      expect(subject.endpoint).to eq 'http://example.org'
-    end
-
-    it 'should return the default user agent' do
+    it 'returns the default user agent' do
       expect(subject.user_agent).to eq TheShiningSource::Configuration::DEFAULT_USER_AGENT
     end
 
-    it 'should allow to set new user agent' do
+    it 'sets the default mime type to JSON' do
+      expect(subject.mime_type).to eq :json
+    end
+
+  end
+    
+  describe 'setting configuration options' do
+    
+    it 'can set the adapter' do
+      subject.adapter = :typhoeus
+      expect(subject.adapter).to eq :typhoeus
+    end
+
+    it 'can set the endpoint' do
+      subject.endpoint = 'http://example.org'
+      expect(subject.endpoint).to eq 'http://example.org'
+    end
+    
+    it 'set the user agent' do
       subject.user_agent = 'New User Agent'
       expect(subject.user_agent).to eq 'New User Agent'
     end
 
-    it 'should have set mime type to json' do
-      expect(subject.mime_type).to eq :json
-    end
-    
-    it 'should allow to set current api client' do
+    it 'can set the current api client' do
       expect(subject).to respond_to :client=
       expect(subject).to respond_to :client
     end
@@ -68,7 +72,7 @@ describe TheShiningSource do
   
   describe '.configure' do
     TheShiningSource::Configuration::VALID_OPTIONS_KEYS.each do |key|
-      it "should set the #{key}" do
+      it "sets the #{key} directly" do
         TheShiningSource.configure do |config|
           config.send("#{key}=", key)
           expect(TheShiningSource.send(key)).to eq key
