c5dc392246e03cbf896ba9a7315ade2df45330a9
Phil Newton <phil@sodaware.net>
2015-08-16 07:02:54 -0400

    Response now passes tests again

--- lib/the_shining_source/response.rb
+++ lib/the_shining_source/response.rb
@@ -1,17 +1,21 @@
 module TheShiningSource
   module Response
     
+    attr_accessor :pagination
+    attr_accessor :meta
+
     def self.create(response_hash)
       data = response_hash.data.dup rescue response_hash
       data.extend(self)
-      data.instance_exec do
-        @pagination = response_hash.pagination
-        @meta = response_hash.meta
-      end
+      
+      # [todo] - Do we even really need this?
+      # data.instance_exec do
+      #   @pagination = response_hash.pagination
+      #   @meta = response_hash.meta
+      # end
+      
       data
     end
 
-    attr_reader :pagination
-    attr_reader :meta
   end
 end
